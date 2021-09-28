238a4921c0556c9a79d21af0cc08dd3c431e1e24
amrmohamed25 <amrmohamedawed80@gmail.com>
2021-09-29 01:22:06 +0200

    added shared folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
-
-
-
-
-
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_app/shared/components/components.dart';
+import 'package:social_app/shared/components/constants.dart';
+import 'package:social_app/shared/network/local/shared_preferences.dart';
+import 'package:social_app/shared/network/remote/dio_remote.dart';
+
+import 'bloc_observer.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+
+  print("Handling a background message: ${message.notification!.title}");
+  buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,31 +24,29 @@ void main() async {
   DioHelper.init();
 
   await Firebase.initializeApp();
-  //
-  // if (Platform.isLinux) await DesktopWindow.setMinWindowSize(Size(400, 650));
-  //
+
   await CacheHelper.init();
   //
-  // var token = await FirebaseMessaging.instance.getToken();
-  // print(token);
-  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-  //   print('Got a message whilst in the foreground!');
-  //   print('Message data: ${message.sentTime}');
-  //
-  //   if (message.notification != null) {
-  //     print('Message also contained a notification: ${message.notification}');
-  //     print(message.data.toString());
-  //     buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
-  //   }
-  // });
-  //
-  // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-  //   print('A new onMessageOpenedApp event was published!');
-  //   print(message.data.toString());
-  //   buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
-  // });
-  //
-  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  var token = await FirebaseMessaging.instance.getToken();
+  print(token);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.sentTime}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+      print(message.data.toString());
+      buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print('A new onMessageOpenedApp event was published!');
+    print(message.data.toString());
+    buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
+  });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   Widget myWidget;
   bool? isDark = CacheHelper.getData('isDark') ?? false;
@@ -44,22 +54,11 @@ void main() async {
   token = CacheHelper.getData('token');
   uId = CacheHelper.getData('uId');
   if (uId != null) {
-    // FirebaseAuth.instance.currentUser!.reload();
     myWidget = SocialLayout();
   } else {
     myWidget = SocialLoginScreen();
   }
-  // myWidget=SocialFeed();
-  // if(finishedBoarding==true){
-  //   if(token!=null){
-  //     myWidget=ShopLayout();
-  //   }
-  //   else{
-  //     myWidget=LoginShopScreen();
-  //   }
-  // }else{
-  //   myWidget=OnBoardingScreen();
-  // }
+
   runApp(MyApp(isDark, myWidget));
 }
 
@@ -73,58 +72,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (BuildContext context) {
-            return AppCubit()
-            // ..createDatabase()
-                ;
-          },
-        ),
-        BlocProvider(
-          create: (BuildContext context) {
-            return NewsCubit()
-              ..getBusiness()
-              ..changeThemeMode(isDark);
-            // ..getBusiness();
-          },
-        ),
-        BlocProvider(
-          create: (BuildContext context) {
-            return ShopCubit()
-              ..getHomeData()
-              ..getCategoryData()
-              ..getFavorites()
-              ..getUserData();
-          },
-        ),
-        BlocProvider(
-          create: (BuildContext context) {
-            return SocialCubit()
-              ..getUserData()
-              ..getPosts();
-          },
-        )
-      ],
-      child: BlocConsumer<AppCubit, AppState>(
-        listener: (BuildContext context, state) {},
-        builder: (BuildContext context, Object? state) {
-          var cubit = AppCubit.get(context);
+
           return MaterialApp(
               debugShowCheckedModeBanner: false,
-              theme: lightTheme,
-              darkTheme: darkTheme,
-              themeMode: cubit.isDark == true ? ThemeMode.dark : ThemeMode.light,
-              home:myWidget
-            // NewsLayout(),
-            // home:SocialLoginScreen(),
-            // home:myWidget,
-            // home: finishedBoarding==true?LoginShopScreen():OnBoardingScreen(),
-            // NewsLayout(),
-          );
-        },
-      ),
-    );
+              home: myWidget
+              );
+
   }
 }

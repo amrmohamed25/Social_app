5f620d0edcb52e26228ceec8cdb312f6c763ec58
amrmohamed25 <amrmohamedawed80@gmail.com>
2021-09-29 01:02:35 +0200

    updated pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,129 @@
+
+
+
+
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  DioHelper.init();
+
+  await Firebase.initializeApp();
+  //
+  // if (Platform.isLinux) await DesktopWindow.setMinWindowSize(Size(400, 650));
+  //
+  await CacheHelper.init();
+  //
+  // var token = await FirebaseMessaging.instance.getToken();
+  // print(token);
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   print('Got a message whilst in the foreground!');
+  //   print('Message data: ${message.sentTime}');
+  //
+  //   if (message.notification != null) {
+  //     print('Message also contained a notification: ${message.notification}');
+  //     print(message.data.toString());
+  //     buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
+  //   }
+  // });
+  //
+  // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  //   print('A new onMessageOpenedApp event was published!');
+  //   print(message.data.toString());
+  //   buildToast(message: message.notification!.body, state: ToastStates.SUCCESS);
+  // });
+  //
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  Widget myWidget;
+  bool? isDark = CacheHelper.getData('isDark') ?? false;
+  bool? finishedBoarding = CacheHelper.getData('boarding') ?? false;
+  token = CacheHelper.getData('token');
+  uId = CacheHelper.getData('uId');
+  if (uId != null) {
+    // FirebaseAuth.instance.currentUser!.reload();
+    myWidget = SocialLayout();
+  } else {
+    myWidget = SocialLoginScreen();
+  }
+  // myWidget=SocialFeed();
+  // if(finishedBoarding==true){
+  //   if(token!=null){
+  //     myWidget=ShopLayout();
+  //   }
+  //   else{
+  //     myWidget=LoginShopScreen();
+  //   }
+  // }else{
+  //   myWidget=OnBoardingScreen();
+  // }
+  runApp(MyApp(isDark, myWidget));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  bool? isDark;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  // bool? finishedBoarding;
+  Widget myWidget;
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  MyApp(this.isDark, this.myWidget);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (BuildContext context) {
+            return AppCubit()
+            // ..createDatabase()
+                ;
+          },
         ),
+        BlocProvider(
+          create: (BuildContext context) {
+            return NewsCubit()
+              ..getBusiness()
+              ..changeThemeMode(isDark);
+            // ..getBusiness();
+          },
+        ),
+        BlocProvider(
+          create: (BuildContext context) {
+            return ShopCubit()
+              ..getHomeData()
+              ..getCategoryData()
+              ..getFavorites()
+              ..getUserData();
+          },
+        ),
+        BlocProvider(
+          create: (BuildContext context) {
+            return SocialCubit()
+              ..getUserData()
+              ..getPosts();
+          },
+        )
+      ],
+      child: BlocConsumer<AppCubit, AppState>(
+        listener: (BuildContext context, state) {},
+        builder: (BuildContext context, Object? state) {
+          var cubit = AppCubit.get(context);
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              themeMode: cubit.isDark == true ? ThemeMode.dark : ThemeMode.light,
+              home:myWidget
+            // NewsLayout(),
+            // home:SocialLoginScreen(),
+            // home:myWidget,
+            // home: finishedBoarding==true?LoginShopScreen():OnBoardingScreen(),
+            // NewsLayout(),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

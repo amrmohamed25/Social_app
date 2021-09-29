8112f961f34a6e2456ece6f458940a5b591eaad8
amrmohamed25 <amrmohamedawed80@gmail.com>
2021-09-29 02:58:02 +0200

    added social message

---
+++ lib/modules/social_message/social_message.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SocialMessage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child:Text('Message page'));
+  }
+}
